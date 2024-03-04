1cb44a34ee32c6ae9b9935646f18946e82f0876c
Guilherme <gui87981@gmail.com>
2024-03-04 11:35:14 -0300

    primeiro commit

---
+++ Black/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Black;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Black/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Black;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
